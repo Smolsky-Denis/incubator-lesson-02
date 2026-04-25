d03bf886aab766092f3d8b88c50a63a56df1f955
Smolsky-Denis <smolskyden@gmail.com>
2026-04-25 12:49:47 +0200

    fixed validation

--- src/entity/posts/validation/posts-dto.validation.ts
+++ src/entity/posts/validation/posts-dto.validation.ts
@@ -5,7 +5,7 @@ const titleValidation = body("title")
     .notEmpty().withMessage('title can not be empty')
     .isString().withMessage('title should be string')
     .trim()
-    .isLength({ min: 1, max: 30 })
+    .isLength({ min: 1, max: 30 }).withMessage('title should be max 30 symbols')
 
 const shortDescriptionValidation = body("shortDescription")
     .exists().withMessage('shortDescription is required')
