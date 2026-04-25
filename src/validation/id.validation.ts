b76cb7875f957f3b9532746a45ba447c5c2f14cb
Smolsky-Denis <smolskyden@gmail.com>
2026-04-25 12:54:39 +0200

    fixed validation

--- src/validation/id.validation.ts
+++ src/validation/id.validation.ts
@@ -1,6 +1,14 @@
 import { param } from "express-validator"
 
+// export const idValidation = param("id")
+//     .exists().withMessage("id is required")
+//     .notEmpty().withMessage("id cannot be empty")
+//     .isInt({min: 0}).withMessage("id must be a positive integer or 0")
+
 export const idValidation = param("id")
     .exists().withMessage("id is required")
+    .bail()
     .notEmpty().withMessage("id cannot be empty")
-    .isInt({min: 0}).withMessage("id must be a positive integer or 0")
+    .bail()
+    .isInt({ min: 1 }).withMessage("id must be a positive integer")
+    .toInt();
