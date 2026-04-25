1d3a3f2e4c6bab07e762c2d1b2a97c32483471c5
Smolsky-Denis <smolskyden@gmail.com>
2026-04-25 13:35:48 +0200

    fixed id delete handlers

--- src/routes/blogsRouter/handlers/delete-blog-by-id.handler.ts
+++ src/routes/blogsRouter/handlers/delete-blog-by-id.handler.ts
@@ -3,11 +3,11 @@ import {HttpStatus} from "../../../core/types/http-statuses";
 import {blogsRepository} from "../../../entity/blogs/repositories/blogs.repository";
 
 export const deleteBlogByIdHandler = (req: Request, res: Response) => {
-    const id: string = req.params.id as string
-    const isDeleted = blogsRepository.deleteBlog(id);
+    const id = req.params.id as string
+    const isDeleted = blogsRepository.deleteBlog(id)
     if (!isDeleted) {
-        return res.status(HttpStatus.NotFound)
+        return res.sendStatus(HttpStatus.NotFound);
     }
 
-    return res.status(HttpStatus.NoContent)
-}
+    return res.sendStatus(HttpStatus.NoContent);
+};
